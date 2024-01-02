4d58ab3e7cc81640b7928b201f9c46e6ba16f4c2
Gerry Jouaud <145365777+GerryJouaud@users.noreply.github.com>
2024-01-02 14:31:34 +0100

    Day1.java

--- Day1.java
+++ Day1.java
@@ -1,83 +1,63 @@
-/*
- * L'idée à travers ce programme est de répondre au problème posé dans le Advent of Code Day 1 (https://adventofcode.com/2023/day/1).
- * Pour le résoudre, j'ai d'abord scanné le fichier dans lequel j'ai inserré tous les inputs.
- */
+package test;
 
-package adventOfCode2023;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+public class Test {
 
-public class Day1 {
+	public static void main(String[] args) throws Exception {
+		String ligne = "";
+		char firstDigit = 0;
+		char lastDigit = 0;
+		String concatDigit;
+		int valeur = 0;
+		int valeurTotale = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String fichierLutins = "D:\\Programmes Pro\\ECLIPSE\\Travaux\\Ressources AdventOfCode\\InputDay1.txt"; // chemin
-																												// du
-																												// fichier
-		File donneesLutins = new File(fichierLutins); // instance de la classe File
+		Map<String, String> traducteur = new HashMap<>();
+		traducteur.put("one", "1");
+		traducteur.put("two", "2");
+		traducteur.put("three", "3");
+		traducteur.put("four", "4");
+		traducteur.put("five", "5");
+		traducteur.put("six", "6");
+		traducteur.put("seven", "7");
+		traducteur.put("eight", "8");
+		traducteur.put("nine", "9");
 
-		List<String> listeDonnees = new ArrayList<>();
+		BufferedReader br = new BufferedReader( // création buffered Reader pour lire le fichier
+				new FileReader("D:\\Programmes Pro\\ECLIPSE\\Travaux\\Ressources AdventOfCode\\InputDay1.txt")); // chemin
+		while ((ligne = br.readLine()) != null) {
 
-		try {
+			System.out.println(ligne); // Affichage de la ligne avant traduction
 
-			Scanner analyseDonnees = new Scanner(donneesLutins); // création d'une instance de scanner pour lire le
-																	// fichier
-			while (analyseDonnees.hasNextLine()) {
-				String ligne = analyseDonnees.nextLine(); // chaque ligne est parcourue
-
-				Pattern regexChiffres = Pattern.compile("\\D*(\\d+)\\D*"); // regx qui recherche un ou plusieurs
-																			// chiffres
-				Matcher triChiffres = regexChiffres.matcher(ligne); // Applique cette regx à chaque ligne
-
-				StringBuilder concatenatedNumbers = new StringBuilder(); // Utiliser StringBuilder pour concaténer les
-																			// chiffres
-
-				while (triChiffres.find()) {
-					String chiffre = triChiffres.group(1); // Utiliser le groupe 1 pour obtenir le chiffre capturé
-					concatenatedNumbers.append(chiffre);
-				}
-
-				// Ajouter le résultat (nombre concaténé) à la liste
-				listeDonnees.add(concatenatedNumbers.toString());
+			for (Map.Entry<String, String> entry : traducteur.entrySet()) {
+				ligne = ligne.replace(entry.getKey(), (entry.getValue()));
 			}
+			System.out.println(ligne);
 
-			analyseDonnees.close(); // on ferme le ficher
+			for (char c : ligne.toCharArray()) {
+				// if
 
-			List<Integer> listeDonneesFinale = new ArrayList<>();
-			for (String element : listeDonnees) {
-				if (element.length() >= 2) { // On trie les elements qui contiennent + de 2 chiffres
-					String premierChiffre = element.substring(0, 1);
-					String dernierChiffre = element.substring(element.length() - 1);
-					String donneeFinale = premierChiffre + dernierChiffre;
-
-					listeDonneesFinale.add(Integer.parseInt(donneeFinale)); // et on convertit en INT
-				} else {
-					listeDonneesFinale.add(Integer.parseInt(element)); // et on convertit en INT
+				if (Character.isDigit(c)) {
+					firstDigit = c;
+					break;
 				}
 			}
 
-			int totalDonnees = 0;
-			for (int element : listeDonneesFinale) {
-				if (element < 10) {
-					totalDonnees += element * 11; // Ainsi, si l'élement fait 9, renvoie 99
-				} else {
-					totalDonnees += element;
+			for (int index = ligne.length() - 1; index >= 0; index--) {
+				char c = ligne.charAt(index);
+				if (Character.isDigit(c)) {
+					lastDigit = c;
+					break;
 				}
 			}
-
-			System.out.println(totalDonnees);
-
-		} catch (
-
-		FileNotFoundException e) { // On gère l'exception si le fichier n'est pas trouvé
-			System.err.println("Erreur:" + e.getMessage());
-
+			concatDigit = firstDigit + "" + lastDigit;
+			valeur = Integer.parseInt(concatDigit);
+			valeurTotale += valeur;
+			System.out.println(valeur);
+			System.out.println(valeurTotale);
 		}
 
 	}
